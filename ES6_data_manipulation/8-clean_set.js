35d132100d22eaf9f04543542bde1e0dc7b699f0
Fahd36631 <Fahd36631@gmail.com>
2026-04-02 09:13:33 +0300

    add new task 8

---
+++ ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,16 @@
+export default function cleanSet(set, startString) {
+  if (!startString) {
+    return '';
+  }
+
+  const values = [];
+
+  for (const value of set) {
+    if (typeof value === 'string' && value.startsWith(startString)) {
+      values.push(value.slice(startString.length));
+    }
+  }
+
+  return values.join('-');
+}
+
