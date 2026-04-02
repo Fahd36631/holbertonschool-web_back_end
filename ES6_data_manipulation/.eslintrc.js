2f44b15ef20be94cd31b20049beca7425d4e700e
Fahd36631 <Fahd36631@gmail.com>
2026-04-02 09:03:48 +0300

    add new task 0

---
+++ ES6_data_manipulation/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: { es2021: true, node: true, jest: true },
+  extends: ['eslint:recommended'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  rules: {},
+};
+
