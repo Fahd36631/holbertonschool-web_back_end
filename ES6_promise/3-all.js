f3b40e578c15e95e63864ba4743b9573303ce914
Fahd36631 <Fahd36631@gmail.com>
2026-04-02 09:30:45 +0300

    Fix task 3

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -1,10 +1,10 @@
-import { createUser, uploadPhoto } from './utils.js';
+import { uploadPhoto, createUser } from './utils.js';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((responses) => {
       const [uploadResponse, userResponse] = responses;
-      console.log(uploadResponse.body, userResponse.firstName, userResponse.lastName);
+      console.log(`${uploadResponse.body} ${userResponse.firstName} ${userResponse.lastName}`);
     })
     .catch(() => {
       console.log('Signup system offline');
--- ES6_promise/utils.js
+++ ES6_promise/utils.js
@@ -1,10 +1,8 @@
-export default function uploadPhoto() {
+export function uploadPhoto() {
   return new Promise((resolve) => resolve({ status: 200, body: 'photo-profile-1' }));
 }
 
 export function createUser() {
   return new Promise((resolve) => resolve({ firstName: 'Guillaume', lastName: 'Salva' }));
 }
-
-export { uploadPhoto };
 
