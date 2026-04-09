6c603f45e130dae15e60273762bb9c5b94dfd043
Fahd36631 <Fahd36631@gmail.com>
2026-04-09 09:03:45 +0300

    fix

--- Node_JS_basic/0-console.js
+++ Node_JS_basic/0-console.js
@@ -1,7 +1,5 @@
-// 0-consle.js
-
 function displayMessage(message) {
-    console.log(message);
-  }
-  
-  module.exports = displayMessage;
+  console.log(message);
+}
+
+module.exports = displayMessage;
